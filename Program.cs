c626e1676ff0744049e3556755529d18b5b16258
aryark <aryark3112@gmail.com>
2023-07-26 12:14:02 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoImplementedProperty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            Console.WriteLine("Enter Id");
+            customer.Id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Name");
+            customer.Name = Console.ReadLine();
+            Console.WriteLine("Enter Customer City");
+            customer.City = Console.ReadLine();
+            Console.WriteLine("Enter Customer Over Draft Limit");
+            customer.ODLimit = double.Parse(Console.ReadLine());
+            Console.WriteLine("Customer Details as follows!!");
+            Console.WriteLine($"ID: {customer.Id} Name: {customer.Name} ");
+            Console.WriteLine($"City: {customer.City} ODLimit: {customer.ODLimit}");
+            Console.ReadKey();
+        }
+    }
+}
